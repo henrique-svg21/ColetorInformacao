8c6e6c41fe0ae71db4dfb7915aecca56f81b0889
Henrique Schorck <henrique_schorck@estudante.sesisenai.org.br>
2026-02-10 13:02:50 -0300

    committed

--- src/main/java/LogicaAutomacao/Main.java
+++ src/main/java/LogicaAutomacao/Main.java
@@ -13,15 +13,29 @@ public class Main {
     public static void main(String[] args) {
     //Exibição de cabeçalho: simula a inicialização de um sistema de informação
         System.out.println("_________________________________________________");
-        System.out.println("Sistema de monitoramento industrial");
+        System.out.println("|      SISTEMA DE MONITORAMENTO INDUSTRIAL      |");
         System.out.println("_________________________________________________");
 
     //simulação com laço for
         for (int i = 1; i < 6; i++) {
             System.out.println("Ciclo " + i + " ocorrendo!");
 
+            //ETAPA 1
             double temp = readSensor("Forno 1 temperatura");
+
+            //ETAPA 2 e 3
+            valid_data("Temperature", temp, 20, 80);
+
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                System.out.println("System timer error");
+            }
         }
+
+        System.out.println("_________________________________________________");
+        System.out.println("|               COLETA FINALIZADA               |");
+        System.out.println("_________________________________________________");
     }
 
     //metodo especial
@@ -29,5 +43,16 @@ public class Main {
         Random num_gen = new Random();
         double ran_value = 10 + (100 - 10) * num_gen.nextDouble();
         return ran_value;
+    }
+
+    public static void valid_data (String sensorName, double value, double minV, double maxV) {
+        //show value with 2 decimal places
+        System.out.printf("Sensor: %s | Current value: %.2f °C", sensorName, value);
+        if (value >= minV && value <= maxV) {
+            System.out.println(" >> Operating normally! <<");
+        }
+        else {
+            System.out.println(" >> AWARE, OUT OF SAFE LEVELS! <<");
+        }
     }
 }
